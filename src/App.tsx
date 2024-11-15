db67004451b4732dfa0688d26db773376cf77140
WAN JIA XING <jiaxing.wan@redone.com.my>
2024-11-15 16:52:33 +0800

    Added Footer

--- src/App.tsx
+++ src/App.tsx
@@ -15,27 +15,27 @@ const App = () => (
     <Nav />
     <section className="xl:padding-l wide:padding-r padding-b">
       <Hero />
-      <section className="padding">
-        <PopularProducts />
-      </section>
-      <section className="padding">
-        <SuperQuality />
-      </section>
-      <section className="padding-x py-10">
-        <Services />
-      </section>
-      <section className="padding">
-        <SpecialOffer />
-      </section>
-      <section className="padding bg-pale-blue">
-        <CustomerReviews />
-      </section>
-      <section className="padding-x sm:py-32 py-16 w-full">
-        <Subscribe />
-      </section>
-      <section className="padding-x padding-y bg-black pb-8">
-        <Footer />
-      </section>
+    </section>
+    <section className="padding">
+      <PopularProducts />
+    </section>
+    <section className="padding">
+      <SuperQuality />
+    </section>
+    <section className="padding-x py-10">
+      <Services />
+    </section>
+    <section className="padding">
+      <SpecialOffer />
+    </section>
+    <section className="padding bg-pale-blue">
+      <CustomerReviews />
+    </section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe />
+    </section>
+    <section className="padding-x padding-y bg-black pb-8">
+      <Footer />
     </section>
   </main>
 )
---
+++ src/types/FooterLink.ts
@@ -0,0 +1,9 @@
+export interface footerLinks {
+  title: string
+  links: [footerItem]
+}
+
+interface footerItem {
+  name: string
+  link: string
+}
